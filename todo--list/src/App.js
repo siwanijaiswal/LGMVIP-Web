ce997242cfad597a609522371d97127854b6fd61
siwanijaiswal <siwani.jce20@sot.pdpu.ac.in>
2022-08-07 00:27:34 +0530

    version 1.0

---
+++ todo--list/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="Todo-list-container">
+     <TodoList></TodoList> 
+      
+    </div>
+  );
+}
+
+export default App;
